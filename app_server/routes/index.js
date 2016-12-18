b424b4ffe9340510ddc517974320d0a38b9453b0
JohnVonNeumann <louiswillcock@gmail.com>
2016-12-18 21:01:49 +1100

    created a shit tonne of routes across three separate controllers, one for locations, one for users, one for others(ie:contact, faq, blah blah), ive allocated various route paths across these three contrs and have wondered whether some of them should have nbeen abstracted into separate route folders, and im also 100% certain that I am not done with these routes, but i was starting to stagnate spo figured fuck it, and decided to commit and keep moving

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('..controllers/locations');
+var ctrlOthers = require('..controllers/others');
+var ctrlUsers = require('..controllers/users');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Locations Pages */
+router.get('/', ctrlLocations.home);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/location/directory', ctrlLocations.directory);
+router.get('/location/getListed', ctrlLocations.newListing);
+
+
+/* Others Pages */
+router.get('/about', ctrlOthers.about);
+router.get('/blog', ctrlOthers.blog);
+router.get('/contact', ctrlOthers.contact);
+router.get('/faq', ctrlOthers.faq);
+
+
+/* Users Pages */
+router.get('/users/login', ctrlUsers.login);
+router.get('/users/logout', ctrlUsers.logout);
+router.get('/users/signup', ctrlUsers.signup);
+router.get('/users/signup/confirm', ctrlUsers.signupConfirm);
+router.get('/users/profile', ctrlUsers.profile);
+router.get('/users/profile/new', ctrlUsers.profileNew);
+router.get('/users/profile/favourites', ctrlUsers.favourites);
 
 module.exports = router;
